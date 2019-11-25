ef4d4a2fc6f67f4a09ddb427c782baec867ce7c8
DavidB00371492 <55994218+DavidB00371492@users.noreply.github.com>
2019-11-25 19:31:46 +0000

    Revert "Refining Prototype and finalized first level"
    This reverts commit 7cf2d00ecd9b8ec871928ccc49979fae98e8a329.

--- Medieval-Escape/Assets/Scripts/Patrol.cs
+++ Medieval-Escape/Assets/Scripts/Patrol.cs
@@ -6,11 +6,14 @@ public class Patrol : MonoBehaviour
 {
     public float speed;
     public float distance;
+
     public float health = 1;
+
     private bool movingRight = true;
+
     public Transform groundDetection;
 
-    //Removes health from the enemy when ran
+    // Start is called before the first frame update
     public void TakeDamage()
     {
         health -= 1;
@@ -18,7 +21,6 @@ public class Patrol : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        //If the enemy collides with an anvil, they take damage
         if(collision.gameObject.tag == "Anvil")
         {
             Destroy(collision.collider.gameObject);
@@ -30,19 +32,15 @@ public class Patrol : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       //If the enemies health reaches zero, they are destroyed
        if(health <= 0)
         {
             Destroy(gameObject);
         }
 
-       //Moves the player's transform right at the given speed by default
         transform.Translate(Vector2.right * speed * Time.deltaTime);
 
-        //Projects a raycast from the enemy plus the given distance downwards for detecting ground
         RaycastHit2D groundInfo = Physics2D.Raycast(groundDetection.position, Vector2.down, distance);
 
-        //If there is no collision with the ground and the enemy is moving right, the enemy is flipped otherwise the enemy is flipped in the opposite direction
         if (groundInfo.collider == false)
         {
             if (movingRight == true)
